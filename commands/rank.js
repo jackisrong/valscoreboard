13eb6a2f4c3ef21ecf0991ac6cbf2927fd9f79ab
jackisrong <jackrongjr@hotmail.com>
2023-01-23 23:22:29 -0800

    Add API error handling

--- commands/rank.js
+++ commands/rank.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { request } from 'undici';
 
 export const data = new SlashCommandBuilder()
@@ -11,25 +11,49 @@ export const data = new SlashCommandBuilder()
 	.addStringOption(option =>
 		option.setName('tag')
 			.setDescription('Player\'s tagline after the hashtag (ex. 0000)')
-			.setRequired(true))
-	.addStringOption(option =>
-		option.setName('region')
-			.setDescription('Player\'s account region (default Americas)')
-			.addChoices(
-				{ name: 'Americas', value: 'na' },
-				{ name: 'Europe', value: 'eu' },
-				{ name: 'Asia-Pacific', value: 'ap' },
-				{ name: 'Korea', value: 'kr' },
-			));
+			.setRequired(true));
 
 export async function execute(interaction) {
-	const region = interaction.options.getString('region') ?? 'na';
 	const username = interaction.options.getString('username');
 	const tag = interaction.options.getString('tag');
 
 	await interaction.deferReply();
-	const req = await request(`https://api.henrikdev.xyz/valorant/v2/mmr/${region}/${username}/${tag}`);
-	const res = (await req.body.json()).data;
-	const reply = `${username}#${tag}'s current rank is ${res.current_data.currenttierpatched}. Their peak rank was ${res.highest_rank.patched_tier} during ${(res.highest_rank.season).toUpperCase()}.`;
-	await interaction.editReply(reply);
+
+	const reqAcc = await request(`https://api.henrikdev.xyz/valorant/v1/account/${username}/${tag}`);
+	const resAcc = await reqAcc.body.json();
+	if (resAcc.status != 200) {
+		await interaction.editReply({ content: `There was an error while executing this command: ${resAcc.errors[0].message}` });
+		return;
+	}
+	const dataAcc = resAcc.data;
+
+	const req = await request(`https://api.henrikdev.xyz/valorant/v2/mmr/${dataAcc.region}/${username}/${tag}`);
+	const res = await req.body.json();
+	if (res.status != 200) {
+		await interaction.editReply({ content: `There was an error while executing this command: ${res.errors[0].message}` });
+		return;
+	}
+	const d = res.data;
+
+	const embed = new EmbedBuilder()
+		.setColor(0xcd7dff)
+		.setAuthor({ name: 'Rank History' })
+		.setTitle(`${username}#${tag}`)
+		.setThumbnail(dataAcc.card.small)
+		.addFields(
+			{ name: 'Current Rank', value: d.current_data.currenttierpatched },
+			{ name: 'Peak Rank', value: `${d.highest_rank.patched_tier} (${(d.highest_rank.season).toUpperCase()})` },
+			{ name: '\u200B', value: '\u200B' },
+		)
+		.setTimestamp();
+
+	for (const key in d.by_season) {
+		if (d.by_season[key].error) {
+			embed.addFields({ name: `${key.toUpperCase()}`, value: 'Unranked', inline: true });
+		} else {
+			embed.addFields({ name: `${key.toUpperCase()}`, value: d.by_season[key].final_rank_patched, inline: true });
+		}
+	}
+
+	await interaction.editReply({ embeds: [embed] });
 }
